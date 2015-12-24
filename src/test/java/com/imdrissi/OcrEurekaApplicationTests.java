f8e253c17998d3949a0be4ea1e3ce85b6ed595bd
islam drissi <zirconias777@yahoo.com>
2015-12-24 23:14:54 +0100

    first commit

---
+++ src/test/java/com/imdrissi/OcrEurekaApplicationTests.java
@@ -0,0 +1,18 @@
+package com.imdrissi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = OcrEurekaApplication.class)
+@WebAppConfiguration
+public class OcrEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
